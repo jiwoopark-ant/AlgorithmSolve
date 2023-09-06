22eff9b730a7bdb64776cf7e6811b04832c4700d
jiwoopark-ant <79710846+jiwoopark-ant@users.noreply.github.com>
2023-09-06 21:35:41 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.py
@@ -0,0 +1,20 @@
+def solution(arr):
+    
+#     for i in arr:
+#         if arr.index(i) == arr.index(min(arr)):
+#             del arr[i]
+#         answer= arr    
+# #     arr.sort(reverse=True)
+# #     arr.pop()
+# #     if arr==[]:
+# #         arr.append(-1)
+            
+    
+#     return arr
+    arr.remove(min(arr))
+    if arr==[]:
+        answer=arr.append(-1)
+    else:   
+        answer=arr
+
+    return arr    
